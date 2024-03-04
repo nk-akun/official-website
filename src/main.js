f82161a260c7e5fd88f2b2b41e5e8e6b7b4b5fc4
Marongkun <marongkun.kwn@bytedance.com>
2024-03-04 20:48:48 +0800

    feat: http

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ axios.defaults.timeout = 5000
 // axios.defaults.baseURL = 'http://192.168.1.9:5001/api/'
 // axios.defaults.baseURL = 'http://127.0.0.1:5001/api/'
 // axios.defaults.baseURL = 'http://43.138.21.182/api/'
-axios.defaults.baseURL = 'https://fgby1688.com/api/'
+axios.defaults.baseURL = 'http://fgby1688.com/api/'
 Vue.prototype.$http = axios
 
 // 图片服务器地址
@@ -48,7 +48,7 @@ Vue.prototype.$http = axios
 // Vue.prototype.imgserver = 'http://192.168.1.9:8000/image'
 // Vue.prototype.imgserver = 'http://127.0.0.1:8000/image'
 // Vue.prototype.imgserver = 'http://43.138.21.182/image'
-Vue.prototype.imgserver = 'https://fgby1688.com/image'
+Vue.prototype.imgserver = 'http://fgby1688.com/image'
 
 // new Vue({
 //   router,
